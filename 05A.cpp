c3649af423a8070f299c62f8a219f4bd09bb343d
jebouin <jebouin@gmail.com>
2017-12-09 17:33:47 +0100

    day 5

---
+++ 05A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+const int N = 5555;
+int a[N];
+int n;
+
+int main() {
+    while(cin >> a[n]) {
+        n++;
+    }
+    int pos = 0, ans = 0;
+    while(pos < n) {
+        int np = pos + a[pos];
+        a[pos]++;
+        pos = np;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
